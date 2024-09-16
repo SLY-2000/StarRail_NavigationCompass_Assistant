5adcef1f669db6cba517660ab83725b52c88570d
longyisong <1216568072@qq.com>
2024-09-17 00:21:31 +0800

    添加项目文件。

---
+++ Navigation Compass/Navigation Compass.cpp
@@ -0,0 +1,42 @@
+//#include "iostream.h"
+/*
+* x:轮盘1和轮盘2联动旋转次数
+* y:轮盘1和轮盘3联动旋转次数
+* z:轮盘2和轮盘3联动旋转次数
+* a:轮盘1和终点距离格子数
+* b:轮盘2和终点距离格子数
+* c:轮盘3和终点距离格子数
+* (1*(x+y))%6=a
+* (2*(x+z))%6=b
+* (4*(y+z))%6=c
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include "stdio.h"
+int main()
+{
+	unsigned int x, y, z, a, b, c;
+	printf("\r\n轮盘1和终点距离格子数:");
+	scanf_s("%d",&a);
+	printf("\r\n轮盘2和终点距离格子数:");
+	scanf_s("%d",&b);
+	printf("\r\n轮盘3和终点距离格子数:");
+	scanf_s("%d",&c);
+	for (z = 0; z < 100; z++)
+	{
+		for (y = 0; y < 100; y++)
+		{
+			for (x = 0; x < 100; x++)
+			{
+				if (((4 * (y + z)) % 6 == c) && ((2 * (x + z)) % 6 == b) && ((1 * (x + y)) % 6 == a))
+				{
+
+					printf("\r\n轮盘1和轮盘2联动旋转次数：%d", x);
+					printf("\r\n轮盘1和轮盘3联动旋转次数：%d", y);
+					printf("\r\n轮盘2和轮盘3联动旋转次数：%d", z);
+					return 0;
+				}
+			}
+		}
+	}
+	
+}
